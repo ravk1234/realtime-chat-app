82af38e039eedde71af41f3dd3c5014e53142ec6
ravk1234 <rkant4111@gmail.com>
2022-06-20 15:50:36 +0530

    feature add create and delete user operations

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const io = require('socket.io')(3000)
 
+const users = {}
 io.on('connection',socket =>{
-    console.log("user")
-    socket.emit('chat-message','hello')
+   
+    socket.on('new-user', data=>{
+        users[socket.id] = data
+        socket.broadcast.emit('user-connected',data)
+    })
+    socket.on('send-chat-message',message =>{
+        socket.broadcast.emit('chat-message',{message:message,name:users[socket.id]})
+        console.log(message)
+    })
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+      })
 })
